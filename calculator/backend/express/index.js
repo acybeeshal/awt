052a0d1c81df0936bcb1ad574502b7bc9acfff03
acybeeshal <acybeeshal@gmail.com>
2025-05-23 06:44:19 +0545

    note update

---
+++ calculator/backend/express/index.js
@@ -0,0 +1,9 @@
+import app from './app';
+
+const db = require('better-sqlite3')('data.db');
+db.prepare(`CREATE TABLE IF NOT EXISTS quotes(
+    id INTEGER PPRIMARY KEY AUTOINCREMENT ,
+    text TEXT NOT NULL
+)`).run();
+
+app.get('/quotes')
